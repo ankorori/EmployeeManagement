939a6667b5490bdc6e8a07da41a9d6ba41671065
miyagawatakashi <miyagawa@hanjou.co.jp>
2022-01-26 13:50:08 +0900

    ログイン時の最初に表示されるページ変更

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/pcAccounts';
 
     /**
      * The controller namespace for the application.
