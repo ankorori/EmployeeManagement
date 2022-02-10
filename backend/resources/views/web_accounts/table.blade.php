1d0cc60690d402a33d92980c591f087c384458e4
miyagawatakashi <miyagawa@hanjou.co.jp>
2022-02-10 14:26:31 +0900

    中間テーブルの登録、削除処理実装

--- backend/resources/views/web_accounts/table.blade.php
+++ backend/resources/views/web_accounts/table.blade.php
@@ -21,7 +21,7 @@
                     <div class='btn-group'>
                         <a href="{{ route('webAccounts.show', [$webAccount->id]) }}" class='btn btn-ghost-success'><i class="fa fa-eye"></i></a>
                         <a href="{{ route('webAccounts.edit', [$webAccount->id]) }}" class='btn btn-ghost-info'><i class="fa fa-edit"></i></a>
-                        {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-ghost-danger', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                        {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-ghost-danger', 'onclick' => "return confirm('{{ $webAccount->name }}を削除してよろしいですか?')"]) !!}
                     </div>
                     {!! Form::close() !!}
                 </td>
