d5f2a6a87fc2f8ee7756d0f4824e536917dbfce9
miyagawatakashi <miyagawa@hanjou.co.jp>
2022-03-04 13:16:28 +0900

    インデント修正 該当PCアカウントへのリンクを追加

--- backend/resources/views/devices/table.blade.php
+++ backend/resources/views/devices/table.blade.php
@@ -3,19 +3,19 @@
         <thead>
             <tr>
                 <th>Devics Number</th>
-        <th>Company</th>
-        <th>Pc Name</th>
-        <th>Pc Account Id</th>
-        <th>Ostype</th>
-        <th>Is Cd Dvd Drive</th>
-        <th>Is Wired Lan</th>
-        <th>Is Wireless Lan</th>
-        <th>Is Internet</th>
-        <th>Is Taking Out</th>
-        <th>Is Lanscopecat</th>
-        <th>Web Browser Id</th>
-        <th>Mailer</th>
-        <th>Antivirus Software</th>
+                <th>Company</th>
+                <th>Pc Name</th>
+                <th>Pc Account Id</th>
+                <th>Ostype</th>
+                <th>Is Cd Dvd Drive</th>
+                <th>Is Wired Lan</th>
+                <th>Is Wireless Lan</th>
+                <th>Is Internet</th>
+                <th>Is Taking Out</th>
+                <th>Is Lanscopecat</th>
+                <th>Web Browser Id</th>
+                <th>Mailer</th>
+                <th>Antivirus Software</th>
                 <th colspan="3">Action</th>
             </tr>
         </thead>
@@ -23,19 +23,19 @@
         @foreach($devices as $device)
             <tr>
                 <td>{{ $device->devics_number }}</td>
-            <td>{{ $device->company }}</td>
-            <td>{{ $device->pc_name }}</td>
-            <td>{{ $device->pc_account->account_name }}</td>
-            <td>{{ $device->PCos->name }}</td>
-            <td>{{ $device->is_cd_dvd_drive ? '有' : '無'}}</td>
-            <td>{{ $device->is_wired_LAN ? '有' : '無' }}</td>
-            <td>{{ $device->is_wireless_LAN ? '有' : '無' }}</td>
-            <td>{{ $device->is_internet ? '有' : '無' }}</td>
-            <td>{{ $device->is_taking_out ? '有' : '無' }}</td>
-            <td>{{ $device->is_LanScopeCat ? '有' : '無' }}</td>
-            <td>{{ $device->web_browser_id }}</td>
-            <td>{{ $device->mailer }}</td>
-            <td>{{ $device->antivirus_software }}</td>
+                <td>{{ $device->company }}</td>
+                <td>{{ $device->pc_name }}</td>
+                <td><a href="{{ route('pcAccounts.show', [$device->pc_account->id]) }}">{{ $device->pc_account->account_name }}</a></td>
+                <td>{{ $device->PCos->name }}</td>
+                <td>{{ $device->is_cd_dvd_drive ? '有' : '無'}}</td>
+                <td>{{ $device->is_wired_LAN ? '有' : '無' }}</td>
+                <td>{{ $device->is_wireless_LAN ? '有' : '無' }}</td>
+                <td>{{ $device->is_internet ? '有' : '無' }}</td>
+                <td>{{ $device->is_taking_out ? '有' : '無' }}</td>
+                <td>{{ $device->is_LanScopeCat ? '有' : '無' }}</td>
+                <td>{{ $device->web_browser_id }}</td>
+                <td>{{ $device->mailer }}</td>
+                <td>{{ $device->antivirus_software }}</td>
                 <td>
                     {!! Form::open(['route' => ['devices.destroy', $device->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
